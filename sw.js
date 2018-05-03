d56696fe66ce590a636f544c883da2b815c4fc95
Johannes Drechsler <JohannesDrechslerUS@gmail.com>
2018-05-03 18:15:40 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,20 +10,24 @@ const staticVendorFiles = [
 	'https://unpkg.com/vue@2.5.16/dist/vue.js',
 	'https://unpkg.com/http-vue-loader@1.3.4/src/httpVueLoader.js',
 	'https://unpkg.com/quasar-framework@%5E0.15.1/dist/umd/quasar.mat.umd.min.js',
-	'https://www.gstatic.com/firebasejs/4.8.1/firebase.js',
-	'https://www.gstatic.com/firebasejs/4.6.2/firebase-firestore.js',
+	'https://unpkg.com/firebase@4.8.1/firebase.js',
+	'https://unpkg.com/firebase@4.6.2/firebase-firestore.js',
 	'https://unpkg.com/vue-firestore@0.2.7/dist/vue-firestore.js'
 ];
 
-self.addEventListener('install', (/**@type{Event} */ event) => {
+self.addEventListener('install', (/**@type{ExtendableEvent} */ event) => {
 	console.log('[SW] Installing service worker.');
 	console.log('Caching static vendor files that are unlikely to change...');
 
-	caches.open(staticVendorCache, (/**@type{Cache} */ cache) => {
-		return cache
-			.addAll(staticVendorFiles)
-			.then(console.log('Done caching static vendor files.'));
-	});
+	event.waitUntil(
+		caches
+			.open(staticVendorCache)
+			.then((/**@type{Cache} */ cache) => {
+				return cache
+					.addAll(staticVendorFiles)
+					.then(console.log('Done caching static vendor files.'));
+			})
+	);
 });
 
 self.addEventListener('activate', event => {
@@ -31,5 +35,11 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', (/**@type{FetchEvent} */ event) => {
-	// console.log(`Fetching: ${event.request.url}`);
+	event.respondWith(caches.open(staticVendorCache)
+		.then((/**@type{Cache} */ cache) => {
+			return cache.match(event.request).then((/**@type{Response} */ response) => {
+				return response || fetch(event.request);
+			});
+		})
+	);
 });
